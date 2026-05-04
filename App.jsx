54e2017e1203663ee8528f3c7024a1c75027ac1d
문승욱 <uoowoq@gmail.com>
2026-05-04 18:32:21 +0900

    project3

--- App.jsx
+++ App.jsx
@@ -1,49 +1,9 @@
-import { useState, useRef } from "react";
 import "./App.css";
-import Header from "./component/Header";
-import TodoEditor from "./component/TodoEditor";
-import TodoList from "./component/TodoList";
-
-const mockTodo = [
-  {
-    id: 0,
-    isDone: false,
-    content: "React 공부하기",
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 1,
-    isDone: false,
-    content: "빨래널기",
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 2,
-    isDone: false,
-    content: "노래 연습하기",
-    createdDate: new Date().getTime(),
-  },
-];
 
 function App() {
-  const [todo, setTodo] = useState(mockTodo);
-  const idRef = useRef(3);
-  const onCreate = (content) => {
-    const newItem = {
-      id: idRef.current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
-    idRef.current += 1;
-  };
-
   return (
     <div className="App">
-      <Header />
-      <TodoEditor onCreate={onCreate} />
-      <TodoList todo={todo} />
+      <h1>감정 일기장</h1>
     </div>
   );
 }
